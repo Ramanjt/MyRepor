e9f39432670dd61da649487914dc6c3e9a8ef619
Raman26 <60250702+Raman26@users.noreply.github.com>
2020-06-26 17:47:13 -0400

    Product Model and InMemory Product Repository Built

---
+++ MyRepor/MyRepor.Core/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRepor.Core.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+    }
+}
