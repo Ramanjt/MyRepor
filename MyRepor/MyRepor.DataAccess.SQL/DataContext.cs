448df93774630c1c0a1c6ec18db2aafc091f604a
Raman26 <60250702+Raman26@users.noreply.github.com>
2020-07-15 16:26:06 -0400

    Adding Entity Framework

---
+++ MyRepor/MyRepor.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using MyRepor.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRepor.DataAccess.SQL
+{
+    public class DataContext: DbContext
+    {
+        public DataContext()
+            :base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
---
+++ MyRepor/MyRepor.DataAccess.SQL/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace MyRepor.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MyRepor.DataAccess.SQL.DataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MyRepor.DataAccess.SQL.DataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
