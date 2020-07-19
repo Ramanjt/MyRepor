be0df1b51d51d2d32322cbd0b7e8f82899614fe2
Raman26 <60250702+Raman26@users.noreply.github.com>
2020-07-19 19:59:24 -0400

    Filter added in product list

---
+++ MyRepor/MyRepor.Core/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MyRepor.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRepor.Core.ViewModels
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
